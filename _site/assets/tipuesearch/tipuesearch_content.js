3f10dbaffb4bacb3f496bd86cbe018dac194dec8
Seunghun Lee <mr.leesh90@gmail.com>
2021-03-30 11:50:04 +0900

    Auto stash before merge of "master" and "origin/dependabot/bundler/kramdown-2.3.1"

--- _site/assets/tipuesearch/tipuesearch_content.js
+++ _site/assets/tipuesearch/tipuesearch_content.js
@@ -1,7 +1,12 @@
 var tipuesearch = {"pages": [{
+    "title": "NAVER API를 사용해서 크롤링하기",
+    "text": "",
+    "tags": "crawling development",
+    "url": "/development/2021/03/29/crawling_naver_api/"
+  },{
     "title": "selenium을 사용한 데이터 크롤링하기",
-    "text": "사전 설치 1. Selenium &amp; BeautifulSoup설치하기 pip install selenium pip install BeautifulSoup4 2. 구글 웹 드라이브 설치 &amp; 다운로드 Selenium을 사용해서 크롤링을 하기 위해서는 웹 드라이브가 필요합니다. 저는 구글 웹 드라이브를 사용해보겠습니다. 구글 웹 드라이브 다운로드 홈페이지 : https://chromedriver.chromium.org/downloads 다운로드 홈페이지에 접속하면 아래와 같이 다운을 받을수가 있다. 아래에서 다운을 받을 때, 주의해야하는 점이 있는데 내가 사용하는 크롬의 버전과 일치를 해야한다. 나의 크롬 정보를 살펴보기 위해서 크롬에서 Settings - About Chrome 으로 들어가 확인을 해본다. 크롬의 버전이 89.0.~ 이므로 89 버전의 웹 드라이버를 다운받는다.",
-    "tags": "crawler development",
+    "text": "사전 설치 1. Selenium &amp; BeautifulSoup설치하기 pip install selenium pip install BeautifulSoup4 2. 구글 웹 드라이브 설치 &amp; 다운로드 Selenium을 사용해서 크롤링을 하기 위해서는 웹 드라이브가 필요하다. 나는 구글 웹 드라이브를 사용해본다. 구글 웹 드라이브 다운로드 홈페이지 : https://chromedriver.chromium.org/downloads 다운로드 홈페이지에 접속하면 아래와 같이 다운을 받을수가 있다. 아래에서 다운을 받을 때, 주의 해야 할 부분이 있는데 내가 사용하는 크롬의 버전과 일치를 해야한다. 나의 크롬 정보를 살펴보기 위해서 크롬에서 Settings - About Chrome 으로 들어가 확인을 해본다. 크롬의 버전이 89.0.~ 이므로 89 버전의 웹 드라이버를 다운받는다.",
+    "tags": "crawling development",
     "url": "/development/2021/02/25/crawling_selenium/"
   },{
     "title": "Jupyter Notebook에 가상환경 커널 추가하기",
